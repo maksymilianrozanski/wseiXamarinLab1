f0a81db977a4030a9809594bcb26f49916d54890
Max <maksymilianrozanski@gmail.com>
2020-03-29 16:11:26 +0200

    move slider styles to SliderStyles.cs

--- FirstLab/FirstLab/controls/slider/SliderItem.cs
+++ FirstLab/FirstLab/controls/slider/SliderItem.cs
@@ -1,4 +1,5 @@
 using FirstLab.styles;
+using FirstLab.styles.slider;
 using Xamarin.Forms;
 
 namespace FirstLab.controls.slider
@@ -7,16 +8,16 @@ namespace FirstLab.controls.slider
     {
         public static StackLayout CreateSlider(string name, int min, int max, string unit)
         {
-            var title = new Label {Text = name, Style = SliderLabelStyle()};
+            var title = new Label {Text = name, Style = SliderStyles.SliderLabelStyle()};
 
-            var sliderValue = new Label {Text = min + " " + unit, Style = SliderLabelStyle()};
+            var sliderValue = new Label {Text = min + " " + unit, Style = SliderStyles.SliderLabelStyle()};
 
             var slider = new Slider
             {
                 Maximum = max,
                 Minimum = min,
                 Value = min,
-                Style = SliderStyle()
+                Style = SliderStyles.SliderStyle()
             };
 
             slider.ValueChanged += (sender, args) => { sliderValue.Text = args.NewValue + " " + unit; };
@@ -24,31 +25,6 @@ namespace FirstLab.controls.slider
             return new StackLayout
             {
                 Children = {title, sliderValue, slider}
-            };
-        }
-
-        private static Style SliderLabelStyle()
-        {
-            return new Style(typeof(Label))
-            {
-                Setters =
-                {
-                    new Setter {Property = Label.FontSizeProperty, Value = 20},
-                    new Setter {Property = Label.TextColorProperty, Value = Colors.TextColorMain},
-                }
-            };
-        }
-
-        private static Style SliderStyle()
-        {
-            return new Style(typeof(Label))
-            {
-                Setters =
-                {
-                    new Setter {Property = Slider.ThumbColorProperty, Value = Colors.AccentColorPrimary},
-                    new Setter {Property = Slider.MinimumTrackColorProperty, Value = Colors.AccentColorPrimary},
-                    new Setter {Property = Slider.MaximumTrackColorProperty, Value = Colors.AccentColorSecondary}
-                }
             };
         }
     }
---
+++ FirstLab/FirstLab/styles/slider/SliderStyles.cs
@@ -0,0 +1,32 @@
+using Xamarin.Forms;
+
+namespace FirstLab.styles.slider
+{
+    public class SliderStyles
+    {
+        public static Style SliderLabelStyle()
+        {
+            return new Style(typeof(Label))
+            {
+                Setters =
+                {
+                    new Setter {Property = Label.FontSizeProperty, Value = 20},
+                    new Setter {Property = Label.TextColorProperty, Value = Colors.TextColorMain},
+                }
+            };
+        }
+
+        public static Style SliderStyle()
+        {
+            return new Style(typeof(Label))
+            {
+                Setters =
+                {
+                    new Setter {Property = Slider.ThumbColorProperty, Value = Colors.AccentColorPrimary},
+                    new Setter {Property = Slider.MinimumTrackColorProperty, Value = Colors.AccentColorPrimary},
+                    new Setter {Property = Slider.MaximumTrackColorProperty, Value = Colors.AccentColorSecondary}
+                }
+            };
+        }
+    }
+}
