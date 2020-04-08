268e5dd8a5bcc3d27657f9b6d22395fe28b4edfc
Max <maksymilianrozanski@gmail.com>
2020-04-08 13:40:29 +0200

    mode BaseViewModel to separate file

--- FirstLab/FirstLab/viewModels/DetailsViewModel.cs
+++ FirstLab/FirstLab/viewModels/DetailsViewModel.cs
@@ -1,35 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
 using System.Threading;
-using FirstLab.location;
 using Xamarin.Forms;
 
 namespace FirstLab.viewModels
 {
-    public abstract class BaseViewModel : INotifyPropertyChanged
-    {
-        public INavigation Navigation { get; set; }
-        public virtual event PropertyChangedEventHandler PropertyChanged;
-
-        protected BaseViewModel(INavigation navigation)
-        {
-            Navigation = navigation;
-        }
-
-        protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
-        {
-            void NotifyPropertyChanged() =>
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-
-            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
-            field = value;
-            NotifyPropertyChanged();
-            return true;
-        }
-    }
-
     public class DetailsViewModel : BaseViewModel
     {
         public DetailsViewModel(INavigation navigation) : base(navigation)
